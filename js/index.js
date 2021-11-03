3065ac0536c1d19d94c3ec4ee2ff7202950ec641
Kim Hyewon <gpdnjs861@naver.com>
2021-11-03 18:46:52 +0900

    playlist_html_css_js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,44 @@
-var $bar = $('ul.playlist > li > ul.on');
-var $box = $('ul.playlist');
-var total = $bar.length;
-var beginIdx = 0;
-var endIdx = total - 1;
-var barWidth = 0;
-var boxWidth = 0;
-var minOffsetX = -200;
-var contentWidth = $bar.find('li:eq(1)').outerWidth(true);
-var timerId = "";
-/*bar: ul.playlist > li > ul, box: ul.playlist */
-var idx = 1;
-timerId = setInterval(function(){
-	var currentOffsetX = $bar.position().left;
+/*common*/
+$(document).ready(function(){
+	preventDefaultAnchor();
+	showSlide();
+})
 
-	$bar.css({'left': `${currentOffsetX - 1}px`});
-	if (Math.abs(currentOffsetX) > contentWidth * idx)
-	{
-		var $first = $bar.find('li:eq(0)');
-		$first.remove();
-		$bar.append($first);
-		$bar.css({'width': `${3000 + idx * 500}px`});
-		idx++;
-	}
+function preventDefaultAnchor() {
+  $(document).on('click', 'a[href="#"]', function(event) {
+    event.preventDefault();
+  });
+}
+/*image-slide*/
+
+$('.playlist > li > a').on('click', function(){
+	$('.playlist > li').removeClass('on');
+	$(this).parent().addClass('on');
+	showSlide();
+});
+
+function showSlide()
+{
+	var $bar = $('ul.playlist > li.on > ul');
+	var $box = $('ul.playlist');
+	var contentWidth = $bar.find('li:eq(1)').outerWidth(true);
+	var timerId = "";
 	
-}, 30)
+	/*bar: ul.playlist > li > ul, box: ul.playlist */
+	var idx = 1;
+	timerId = setInterval(function(){
+		var currentOffsetX = $bar.position().left;
+	
+		$bar.css({'left': `${currentOffsetX - 0.3}px`});
+		currentOffsetX -= 0.3;
+		if (Math.abs(currentOffsetX) > contentWidth * idx)
+		{
+			var $first = $bar.find('li:eq(0)');
+			$first.remove();
+			$bar.css({'left': `${currentOffsetX + contentWidth}px`});
+			$bar.append($first);
+			$bar.css({'width': `${3000 + idx * 1000}px`});
+			idx++;
+		}
+	}, 8);
+}
