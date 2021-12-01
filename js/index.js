39627e4ea7c49977fc8d9c2eee02748106df49ee
Kim Hyewon <gpdnjs861@naver.com>
2021-12-02 00:39:32 +0900

    js_scroll click event add

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
 /*common*/
 $(document).ready(function(){
 	preventDefaultAnchor();
+  checkScroll();
 });
+
+/*button on/off */
+$('#connection ul.slide li a.control').on('click', function() {
+  $(this).parent().toggleClass('on');
+});
+/*scroll 양에 따라 background 변경*/
+window.on('scroll', ()=>{checkScroll});
 
 function preventDefaultAnchor() {
   $(document).on('click', 'a[href="#"]', function(event) {
     event.preventDefault();
   });
+}
+
+function checkScroll(){
+  var scrollAmt = $(document).scrollTop();
+  if (scrollAmt < 1200)
+     $('body').css({'background': '#FFF'});
+  else
+    $('body').css({'background': '#F5F5F7'});
 }
