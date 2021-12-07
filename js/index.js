555b29529ace37a1f8c25c534b4e99041d604f31
Kim Hyewon <gpdnjs861@naver.com>
2021-12-08 01:12:19 +0900

    mobile gnb_js_ gnb open feature add

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 /*common*/
+if (navigator.userAgent.toLowerCase().indexOf("Trident") > -1)
+{
+  window.location = 'microsoft-edge:' + "https://www.apple.com/kr/airpods/";
+}
+
 $(document).ready(function(){
   preventDefaultAnchor();
   checkScroll();
@@ -6,10 +11,14 @@ $(document).ready(function(){
 
 $(window).on('resize', function(){
   showBanner(bannerNow);
-  if ($(this).width() > 650)
-    $('#main-tab ul').css({'left':`50%`});
+  if ($(this).width() >= 640)
+    $('#main-tab ul').css({'left':'50%'});
   else
     moveMainTab(0);
+  if ($(this).width() >= 1024){
+    $('div.site ul.site-wrapper > li > ul').removeAttr('style');
+    //style속성으로 ul의 height: 0px 이렇게 하면 pc버전에서 문제생김!
+  }
 });
 
 /*button on/off */
@@ -60,8 +69,23 @@ $('#main-tab div.control a:eq(1)').on('click',function(){
   var delX = $(this).parent().prev().outerWidth(true) - $(this).parent().outerWidth(true);
   moveMainTab(-delX);
 });
+var isNavOpen = false;
+$('div.site ul.site-wrapper > li').on('click', function(){
+  var n = $('div.site ul.site-wrapper > li').index($(this));
 
+  if(isNavOpen === false)
+  {
+    $(this).addClass('open');
+    showNav(n);
+  }
+  else
+  {
+    showNav(n);
+    $(this).removeClass('open');
+  }
+});
 /*function */
+
 function preventDefaultAnchor() {
   $(document).on('click', 'a[href="#"]', function(event) {
     event.preventDefault();
@@ -101,3 +125,16 @@ function moveMainTab (delX){
   else
     $('#main-tab div.control').addClass('on');
 }
+
+function showNav(n){
+  var ulHeight = 0;
+  
+  if (isNavOpen === false)
+  {
+    $(`div.site ul.site-wrapper > li:eq(${n}) > ul > li`).each(function(){
+          ulHeight += $(this).outerHeight(true);
+    });
+  }
+  $(`div.site ul.site-wrapper > li:eq(${n}) > ul`).css({'height':`${ulHeight}px`});
+  isNavOpen = isNavOpen ? false : true;
+};
